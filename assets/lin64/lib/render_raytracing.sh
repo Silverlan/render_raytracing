a4d03a183df5a2f819cefe414dd38e26bed5c835
Silverlan <silverlann@gmail.com>
2025-07-30 13:50:18 +0200

    fix: render_raytracing script errors

--- assets/lin64/lib/render_raytracing.sh
+++ assets/lin64/lib/render_raytracing.sh
@@ -2,7 +2,7 @@
 
 # Default PRAGMA_ROOT to "." if not set
 if [ -z "${PRAGMA_ROOT}" ]; then
-  PRAGMA_ROOT="."
+  PRAGMA_ROOT=".."
 fi
 
 "${PRAGMA_ROOT}/lib/render_raytracing" "$@"
--- assets/win64/bin/render_raytracing.bat
+++ assets/win64/bin/render_raytracing.bat
@@ -1,4 +1,4 @@
 @echo off
 
-if "%PRAGMA_ROOT%"=="" set "PRAGMA_ROOT=."
-"%PRAGMA_ROOT%\lib\render_raytracing.exe" %*
+if "%PRAGMA_ROOT%"=="" set "PRAGMA_ROOT=.."
+"%PRAGMA_ROOT%\bin\render_raytracing.exe" %*
